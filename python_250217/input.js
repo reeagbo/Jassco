402878214c0e42a80176483dfabb6093a7150083
reeagbo <48689985+reeagbo@users.noreply.github.com>
2025-02-17 10:08:44 +0100

    Update input.js

--- python_250217/input.js
+++ python_250217/input.js
@@ -1,9 +1,2 @@
 include ("io.asc")
-//include ("math.asc")
-
-function sum(num0) {
-    if (num0 <= 0) return 0; // Base case
-    return num0 + sum(num0 - 1); // Recursive case
-}
-
-console.log ("Sumatory of 10 is (55)= ", sum(10));
+console.log ("hello, world!")
