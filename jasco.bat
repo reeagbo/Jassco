c20c451fdbe474bdae1448f1b33904f8fb99f7ac
reeagbo <48689985+reeagbo@users.noreply.github.com>
2025-02-17 10:13:16 +0100

    Update jasco.bat

--- jasco.bat
+++ jasco.bat
@@ -1,8 +1,8 @@
 cd python
-python main.py ..\%1 ..\output.asm
+python main.py ..\%1 ..\%2
 cd ..
-.\pasmo\pasmo.exe --tap output.asm %2
-"c:\Program Files (x86)\Fuse\fuse.exe" %2
+.\pasmo\pasmo.exe --tap %2 output.tap
+"c:\Program Files (x86)\Fuse\fuse.exe" output.tap
 
 
 
