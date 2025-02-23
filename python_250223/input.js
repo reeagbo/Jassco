1f818a18f8fcd9af385ed061da89dec58da7ca67
reeagbo <48689985+reeagbo@users.noreply.github.com>
2025-02-23 19:33:03 +0100

    Add files via upload

---
+++ python_250223/input.js
@@ -0,0 +1,7 @@
+include ("io.asc")
+
+console.log("Console tests")
+console.log("Output:he100200hellogoodbye")
+console.logchar("h","e")
+console.lognumber(100,200)
+console.logstring("hello", "goodbye")
