f59f2bd4171e164c12a1ca4710ae42bb59637c90
reeagbo <48689985+reeagbo@users.noreply.github.com>
2025-04-23 10:57:48 +0200

    Delete python_250223 directory

--- python_250223/input.js
+++
@@ -1,7 +0,0 @@
-include ("io.asc")
-
-console.log("Console tests")
-console.log("Output:he100200hellogoodbye")
-console.logchar("h","e")
-console.lognumber(100,200)
-console.logstring("hello", "goodbye")
